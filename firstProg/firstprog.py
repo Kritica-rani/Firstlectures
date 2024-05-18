9cd7b76749f0ccf0f555591a2cf1d0799e9e0efb
Kritica Rani <120586121+Kritica-rani@users.noreply.github.com>
2024-05-19 01:45:26 +0530

    Learning python

---
+++ firstProg/firstprog.py
@@ -0,0 +1,7 @@
+print("hello World")
+# Print sum of 2 number by taking input
+
+firstnum = input("Enter first number")
+second = input("Enter second number")
+sum = int(firstnum) + int(second)
+print(sum)
