c07da221e3cbd0918de7cbb75eb1f7091df558bc
Kritica Rani <120586121+Kritica-rani@users.noreply.github.com>
2024-05-22 12:31:04 +0530

    django project

---
+++ dict.py
@@ -0,0 +1,17 @@
+dice = {
+    "name": "Kritica",
+    "lastName": "rani",
+    "num": 50,
+    "tuples": (1, 2, 3),
+    20: 10
+}
+
+# Acces any value in dictionary
+
+print(dice["name"])
+print(dice["num"])
+print(dice[20])
+
+# Updating the value of dictionary
+dice["name"] = "sita",
+print(dice["name"])
---
+++ loops.py
@@ -0,0 +1,11 @@
+count = 0
+while count <= 5:
+    print("hello world")
+    count = count+1
+
+# list value print
+lit = [1, 2, 30, 4, 5]
+i = 0
+while i < len(lit):
+    print(lit[i])
+    i = i+1
